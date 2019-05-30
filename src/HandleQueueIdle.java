67e9e729886494f3962d581c048d827a9dcaa4bd
AlonZolfi <alonzolfi44@gmail.com>
2019-05-30 18:09:26 +0300

    added internet connection region: offline and online classes added handling queue region: HandleQueueIdle and EnterQueue

---
+++ src/HandleQueueIdle.java
@@ -0,0 +1,68 @@
+public class HandleQueueIdle implements IState {
+    @Override
+    public void fileRequest() {
+
+    }
+
+
+
+    @Override
+    public void login() {
+
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public void internetOff() {
+
+    }
+
+    @Override
+    public void internetOn() {
+
+    }
+
+    @Override
+    public void downloadAborted() {
+
+    }
+
+    @Override
+    public void downloadError() {
+
+    }
+
+    @Override
+    public void errorFixed() {
+
+    }
+
+    @Override
+    public void movieOn() {
+
+    }
+
+    @Override
+    public void restartMovie() {
+
+    }
+
+    @Override
+    public void holdMovie() {
+
+    }
+
+    @Override
+    public void movieOff() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+}
---
+++ src/Online.java
@@ -0,0 +1,71 @@
+public class Online implements IState{
+    @Override
+    public void internetOff() {
+
+    }
+
+    @Override
+    public void internetOn() {
+
+    }
+
+
+
+
+
+
+    @Override
+    public void login() {
+
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public void fileRequest() {
+
+    }
+
+    @Override
+    public void downloadAborted() {
+
+    }
+
+    @Override
+    public void downloadError() {
+
+    }
+
+    @Override
+    public void errorFixed() {
+
+    }
+
+    @Override
+    public void movieOn() {
+
+    }
+
+    @Override
+    public void restartMovie() {
+
+    }
+
+    @Override
+    public void holdMovie() {
+
+    }
+
+    @Override
+    public void movieOff() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+}
