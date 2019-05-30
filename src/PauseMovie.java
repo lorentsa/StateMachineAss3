cdffa8f1fc60e75bacea8ee4553fa3f4db143e8a
hilakhadad <hilakhadad@gmail.com>
2019-05-30 17:48:52 +0300

    3 more states

---
+++ src/PauseMovie.java
@@ -0,0 +1,66 @@
+public class PauseMovie implements IState {
+    @Override
+    public void login() {
+        
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public void internetOff() {
+
+    }
+
+    @Override
+    public void internetOn() {
+
+    }
+
+    @Override
+    public void fileRequest() {
+
+    }
+
+    @Override
+    public void downloadAborted() {
+
+    }
+
+    @Override
+    public void downloadError() {
+
+    }
+
+    @Override
+    public void errorFixed() {
+
+    }
+
+    @Override
+    public void movieOn() {
+
+    }
+
+    @Override
+    public void restartMovie() {
+
+    }
+
+    @Override
+    public void holdMovie() {
+
+    }
+
+    @Override
+    public void movieOff() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+}
