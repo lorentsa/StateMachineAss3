fc2550f4c9657578ef740c33edc12adc4488063f
hilakhadad <hilakhadad@gmail.com>
2019-06-02 19:54:10 +0300

    all the options

--- src/Main.java
+++ src/Main.java
@@ -6,51 +6,87 @@ public class Main {
         MovieDownloader movieDownloader = new MovieDownloader();
 
         String s;
-        int i;
         do {
             System.out.println("\nHello User,\n" + "please choose what you want the machine to do: (press only the number of the option)\n" + "1. Turn on\n" + "2. Turn off\n" + "3. Internet on\n" + "4. Internet off\n" + "5. File request\n" + "6. Download aborted\n" + "7. Download error\n" + "8. Error fixed\n" + "9. Movie on\n" + "10. Restart movie\n" + "11. Hold movie\n" + "12. Movie off\n" + "13. Resume\n" +"14. Set storage size\n" +"15. Set file size\n" +"16. Set movie length\n" + "17. Exit\n");
             System.out.print("Your input is:");
             Scanner f = new Scanner(System.in);
             s = f.next();
-            System.out.println("The output is:");
             switch (s) {
                 case "1":
                     movieDownloader.turnOn();
+                    break;
                 case "2":
                     movieDownloader.turnOff();
+                    break;
                 case "3":
                     movieDownloader.internetOn();
+                    break;
                 case "4":
                     movieDownloader.internetOff();
+                    break;
                 case "5":
                     movieDownloader.fileRequest();
+                    break;
                 case "6":
                     movieDownloader.downloadAborted();
+                    break;
                 case "7":
                     movieDownloader.downloadError();
+                    break;
                 case "8":
                     movieDownloader.errorFixed();
+                    break;
                 case "9":
                     movieDownloader.movieOn();
+                    break;
                 case "10":
                     movieDownloader.restartMovie();
+                    break;
                 case "11":
                     movieDownloader.holdMovie();
+                    break;
                 case "12":
                     movieDownloader.movieOff();
+                    break;
                 case "13":
                     movieDownloader.resume();
-                /*case "14":
+                    break;
+                case "14":
                     System.out.println("What is the size you what to set to the storage?");
                     String sizeOfStorage = f.next();
-                    int storageSize=100;
+                    double storageSize=100;
                     try {
-                        storageSize = Integer.parseInt(sizeOfStorage);
+                        storageSize = Double.parseDouble(sizeOfStorage);
                     }
                     catch (Exception e){
                         System.out.println("not a number");
                     }
-                    movieDownloader.setStorage(storageSize);*/
+                    movieDownloader.setStorage(storageSize);
+                    break;
+                case "15":
+                    System.out.println("What is the size you what to set to the file?");
+                    String sizeOfFile = f.next();
+                    double fileSize=10;
+                    try {
+                        fileSize = Double.parseDouble(sizeOfFile);
+                    }
+                    catch (Exception e){
+                        System.out.println("not a number");
+                    }
+                    movieDownloader.setFileSize(fileSize);
+                    break;
+                case "16":
+                    System.out.println("What is the length you what to set to the movie?");
+                    String lntOfMovie = f.next();
+                    int movieLnt=60;
+                    try {
+                        movieLnt = Integer.parseInt(lntOfMovie);
+                    }
+                    catch (Exception e){
+                        System.out.println("not a number");
+                    }
+                    movieDownloader.setStorage(movieLnt);
+                    break;
                 case "17":
                     break;
             }
